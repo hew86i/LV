50c95e494b12b5438d5486693febfffa0668c098
hew86i <h14030@gmail.com>
2014-04-03 12:29:02 +0200

    just a commnet
    commnet for git

--- laravel/app/views/layout/main.blade.php
+++ laravel/app/views/layout/main.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Auth SYSTEM</title>
+	<!-- commnet for git -->
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 	{{ HTML::style('css/style.css')}}
 </head>
